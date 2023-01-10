79ebd33b791c1f0d3f510412b68d18cce70f3dcd
salteax <salteaxdev@protonmail.com>
2023-01-10 20:57:11 +0100

    handle creation of startPacket and exception handling

--- s81801/src/Client.java
+++ s81801/src/Client.java
@@ -2,6 +2,8 @@ import java.net.InetAddress;
 import java.io.*;
 import java.net.*;
 import java.nio.*;
+import java.util.*;
+import java.util.zip.CRC32;
 
 /*
  * 
@@ -22,6 +24,7 @@ public class Client {
         long fileSize = 0;
         InetAddress address = null;
         DatagramSocket socket = null;
+        byte[] startPacket = null;
         
         /* get hostname, exception handling */
         hostname = args[0];
@@ -57,8 +60,10 @@ public class Client {
             System.out.println("\'" + protocol + "\' is not valid, it must be either sw or gbn.");
             System.exit(1);
         }
+        
+        startPacket = createStartPacket(file);
 
-        try {
+        /*try {
             socket = new DatagramSocket();
 
             /*while(true) {
@@ -74,7 +79,7 @@ public class Client {
                 System.out.println(quote + "\n");
 
                 Thread.sleep(10000);
-            }*/
+            }
         } catch(SocketTimeoutException ex) {
             System.out.println("Timeout error: " + ex.getMessage());
             ex.printStackTrace();
@@ -83,10 +88,63 @@ public class Client {
             ex.printStackTrace();
         } catch(InterruptedException ex) {
             ex.printStackTrace();
-        }
+        }*/
     }
 
-    public static void createStartPacket() {}
+    public static byte[] createStartPacket(File file) {
+        int sessionNumber = 0, packetNumber = 0;
+        long fileSize = 0, fileNameSize = 0;
+        String strStartID = "Start";
+        byte[] startID = new byte[5], fileNameByte = null;
+        Random random = null;
+        ByteBuffer startPacketData = null;
+        CRC32 crc32 = null;
+        
+        random = new Random();
+        sessionNumber = random.nextInt(65536);
+
+        try {
+            startID = strStartID.getBytes("US-ASCII");
+        } catch(UnsupportedEncodingException ex) {
+            System.out.println("Problem using charset \'US-ASCII\'.");
+            System.exit(1);
+        }
+
+        fileSize = file.length();
+        fileNameSize = file.getName().length();
+
+        if(file.getName().toLowerCase().matches("^[a-z0-9äöüß_.-]")) {
+            System.out.println("File name \'" + file.getName() + "\' does contain invalid characters.");
+            System.exit(1);
+        }
+
+        try {
+            fileNameByte = file.getName().getBytes("UTF-8");
+        } catch(UnsupportedEncodingException ex) {
+            System.out.println("Problem using charset \'UTF-8\'.");
+            System.exit(1);
+        }
+
+        if(fileNameByte.length > 255) {
+            fileNameByte = Arrays.copyOfRange(fileNameByte, 1, 255);
+        }
+        
+        startPacketData = ByteBuffer.allocate(37 + fileNameByte.length);
+        startPacketData.putInt(sessionNumber);
+        startPacketData.putInt(packetNumber);
+        startPacketData.put(startID);
+        startPacketData.putLong(fileSize);
+        startPacketData.putLong(fileNameSize);
+        startPacketData.put(fileNameByte);
+
+        crc32 = new CRC32();
+        crc32.update(Arrays.copyOfRange(startPacketData.array(), 4, startPacketData.position()));
+        startPacketData.putLong(crc32.getValue());
+    
+        byte[] startPacket = startPacketData.array();
+
+        return startPacket;
+    }
 
     public static void sendStartPacket() {}
 }

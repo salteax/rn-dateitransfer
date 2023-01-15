ab5757575c42b55f6a14ca230b616ed7682017e1
salteax <salteaxdev@protonmail.com>
2023-01-15 02:25:35 +0100

    add file creation, correct minor mistakes

--- s81801/src/Server.java
+++ s81801/src/Server.java
@@ -3,6 +3,7 @@ import java.net.*;
 import java.nio.*;
 import java.util.*;
 import java.util.zip.CRC32;
+import java.nio.charset.StandardCharsets;
 
 public class Server {
     public static void main(String args[]) {
@@ -14,11 +15,14 @@ public class Server {
         int port = 0, delay = 0, fileNameSizeInt = 0, receiveCrc32Int = 0, crc32Int = 0, ack = 0, sessionNumber = 0, packetNumber = 0, i = 0, receivedBytes = 0, sessionNumberData = 0, packetNumberData = 0;
         long fileSizeLong = 0;
         double lossrate = 0;
+        String fileNameString;
         DatagramSocket serverSocket = null;
         InetAddress address = null;
         ByteBuffer byteBuffer = null, fileDataBuffer = null;
-        byte[] receiveData = new byte[1408], dataPacketByte = null, sessionNumberByte = new byte[2], startID = new byte[5], fileSize = new byte[8], fileNameSize = new byte[8], fileName = null, receiveCrc32 = new byte[8], packetNumberByte = new byte[4], returnData = new byte[3], sessionNumberDataByte = new byte[4], packetNumberDataByte = new byte[4];
+        byte[] receiveData = new byte[1408], dataPacketByte = null, sessionNumberByte = new byte[2], startID = new byte[5], fileSize = new byte[8], fileNameSize = new byte[8], fileName = null, receiveCrc32 = new byte[8], packetNumberByte = new byte[4], returnData = new byte[3], sessionNumberDataByte = new byte[4], packetNumberDataByte = new byte[4], dataPacketByteClean = null, fileDataByte = null;
         CRC32 crc32 = null;
+        File file = null;
+        FileOutputStream fileOutputStream = null;
     
         try {
             port = Integer.parseInt(args[0]);
@@ -161,32 +165,22 @@ public class Server {
                 port = receivePacket.getPort();
                 address = receivePacket.getAddress();
 
-                System.out.println("X6");
 
                 dataPacketByte = new byte[receivePacket.getLength()];
                 dataPacketByte = Arrays.copyOfRange(receivePacket.getData(), 0, receivePacket.getLength());
-
-                System.out.println("X5");
            
                 sessionNumberDataByte = Arrays.copyOfRange(dataPacketByte, 0, 4);
                 byteBuffer = ByteBuffer.wrap(sessionNumberDataByte);
                 sessionNumberData = byteBuffer.getInt();
 
-                System.out.println("X3");
-                System.out.println(sessionNumberData);
-
                 packetNumberDataByte = Arrays.copyOfRange(dataPacketByte, 4, 8);
                 byteBuffer = ByteBuffer.wrap(packetNumberDataByte);
                 packetNumberData = byteBuffer.getInt();
-
-                System.out.println("X2");
 
                 System.out.println("Received start packet with sessionnumber \'" + sessionNumberData + "\' and packetnumber \'" + packetNumberData + "\'");
 
                 receiveData = new byte[dataPacketByte.length - 8];
                 receiveData = Arrays.copyOfRange(receivePacket.getData(), 8, receivePacket.getLength());
-
-                System.out.println("X1");
 
                 if(!Arrays.equals(sessionNumberDataByte, sessionNumberByte)) {
                     System.out.println("Sessionnumbers do not equal.");
@@ -206,11 +200,10 @@ public class Server {
 
                 // TODO: Datei abspeichern
 
-
                 if(receivedBytes < (int) fileSizeLong) {
                     fileDataBuffer = ByteBuffer.allocate(receivedBytes);
                     fileDataBuffer.put(receiveData);
-                    receivedBytes = receivedBytes.array();
+                    fileDataByte = fileDataBuffer.array();
 
                     System.out.println("Trying to return data packet.");
                     try {
@@ -222,6 +215,55 @@ public class Server {
                     System.out.println("Returned data packet.");
                 } else {
                     System.out.println("test");
+
+                    dataPacketByteClean = new byte[receiveData.length - 8];
+                    dataPacketByteClean = Arrays.copyOfRange(receiveData, 0, receiveData.length - 8);
+
+                    fileDataBuffer = ByteBuffer.allocate(receivedBytes-8);
+                    fileDataBuffer.put(dataPacketByteClean);
+                    fileDataByte = fileDataBuffer.array();
+
+                    crc32 = new CRC32();
+                    crc32.update(fileDataByte);
+                    
+                    receiveCrc32 = Arrays.copyOfRange(receiveData, receiveData.length-8, receiveData.length);
+                    byteBuffer = ByteBuffer.wrap(receiveCrc32);
+                    receiveCrc32Int = (int) byteBuffer.getLong();
+
+                    if(receiveCrc32Int != (int) crc32.getValue()) {
+                        System.out.println("File CRC32 does not match.");
+                        System.exit(1);
+                    } else {
+                        fileNameString = new String(fileName, StandardCharsets.UTF_8);
+                        fileNameString = fileNameString.substring(0, fileNameString.indexOf('.')) + "1" + fileNameString.substring(fileNameString.indexOf('.'), fileNameString.length());
+
+                        try {
+                            file = new File(fileNameString);
+
+                            fileOutputStream = new FileOutputStream(fileNameString);
+                            fileOutputStream.write(fileDataByte);
+                            fileOutputStream.close();
+                        } catch(FileNotFoundException ex) {
+                            System.out.println("File not found.");
+                            System.exit(1);
+                        } catch(IOException ex) {
+                            System.out.println("Could not create file.");
+                            System.exit(1);
+                        }
+
+                        System.out.println("Created file \'" + fileNameString + "\'.");
+                    }
+
+                    System.out.println("Trying to return data packet.");
+                    try {
+                        serverSocket.send(returnPacket);
+                    } catch(IOException ex) {
+                        System.out.println("Could not send data.");
+                        System.exit(1);
+                    }
+                    System.out.println("Returned data packet.");
+
+                    break;
                 }
             }
         }
